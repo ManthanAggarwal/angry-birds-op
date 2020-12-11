6b31dac95948fa67daafcd26328ee604bc1129f9
Your Name <you@example.com>
2020-12-11 14:59:22 +0530

    c 30

--- AngryBirdsStage2.5StudentActivity-master/sketch.js
+++ AngryBirdsStage2.5StudentActivity-master/sketch.js
@@ -33,26 +33,24 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-   log6 = new Log(200,200,150, PI/7);
-
+   
     bird = new Bird(100,100);
-    sling = new Slingshot(bird.body,{x:100,y:100});
+    sling = new Slingshot(bird.body,{x:200,y:75});
 
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    //console.log(box2.body.position.x);
+   // console.log(box2.body.position.y);
+    //console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
     log1.display();
-    log6.display();
-
+   
     box3.display();
     box4.display();
     pig3.display();
--- AngryBirdsStage2.5StudentActivity-master/slingshot.js
+++ AngryBirdsStage2.5StudentActivity-master/slingshot.js
@@ -1,6 +1,7 @@
 class Slingshot{
 
     constructor(a,b){
+        
     var options={
      bodyA:a, 
      pointB:b,
@@ -9,17 +10,23 @@ class Slingshot{
     }   
     this.sling = Matter.Constraint.create(options);
     World.add(world,this.sling)
-
+    this.sling1 = loadImage("sprites/sling1.png")
+   this.sling2 = loadImage("sprites/sling2.png")
+    this.sling3 = loadImage("sprites/sling3.png")
 
     }
 
     display(){
+        image(this.sling1,200,25)
+        image(this.sling2,170,25)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position
             var pointB = this.sling.pointB
-       line(pointA.x,pointA.y,
-        pointB.x,pointB.y)
-                       
+       line(pointA.x - 30,pointA.y + 20,pointB.x - 20,pointB.y - 15);
+       line(pointA.x - 30,pointA.y + 20,pointB.x + 20,pointB.y - 20);
+     if(pointA.x  < 220 )
+     {image(this.sling3, pointA.x  - 30, pointA.y  - 10 ,15,30)}
+     else{image(this.sling3, pointA.x  + 20, pointA.y  - 10 ,15,30)}                  
 
         }
               
